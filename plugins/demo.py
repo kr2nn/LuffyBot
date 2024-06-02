2bc1bd2162d5697ea4c02041a05072469a85929e
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-02 19:59:49 +0530

    Update demo.py

--- plugins/demo.py
+++ plugins/demo.py
@@ -1,1 +1,38 @@
+import os
+import time
+import psutil
+import shutil
+import string
+import asyncio
+from pyrogram import Client, filters
+from asyncio import TimeoutError
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
+from plugins.config import Config
+from plugins.script import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.database.add import add_user_to_database
+from plugins.functions.forcesub import handle_force_subscribe
 
+
+@Client.on_message(filters.command(["demo"]) & filters.private)
+async def videosdemo(bot, update):
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
+    await bot.send_message(
+        Config.LOG_CHANNEL,
+           f"#PAYMENT💰 : \n\nPay Button Clicked [{update.from_user.first_name}](tg://user?id={update.from_user.id})\n 💰 Payment @{Config.BOT_USERNAME}!!"
+    )
+    
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+    await update.reply_video(
+        video=Translation.DEMO,
+      # caption=Translation.QR_TEXT.format(update.from_user.mention),
+       # disable_web_page_preview=True,
+        reply_markup=Translation.BUTTONS
+    )
