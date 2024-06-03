00d7948752141702f1e82717b5402a2439947897
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-04 03:00:32 +0530

    Create status.py

---
+++ plugins/status.py
@@ -0,0 +1,18 @@
+import traceback, datetime, asyncio, string, random, time, os, aiofiles, aiofiles.os
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+from plugins.database.database import db
+from plugins.config import Config
+
+@Client.on_message(filters.private & filters.command("status"))
+async def status(bot, update):
+    total_users = await db.total_users_count()
+    text = "**Bot Status**\n"
+    text += f"\n**Total Users:** `{total_users}`"
+    await update.reply_text(
+        text=text,
+        quote=True,
+        disable_web_page_preview=True
+    )
