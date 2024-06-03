b5827d5214b6586201d335720eea69de6ad08cee
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-03 22:54:34 +0530

    Update admin_control.py

--- plugins/admin_control.py
+++ plugins/admin_control.py
@@ -1,1 +1,29 @@
+import pyrogram, asyncio, random, time, os
+import time
+import psutil
+import shutil
+import string
+import asyncio
+from pyrogram import Client, filters, enums
+from asyncio import TimeoutError
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
+from plugins.config import Config
+from plugins.script import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.database.add import add_user_to_database
+from plugins.functions.forcesub import handle_force_subscribe
 
+
+@Client.on_message(filters.command('stats') & filters.incoming)
+async def get_ststs(bot, message):
+    rju = await message.reply('<b>Pʟᴇᴀꜱᴇ Wᴀɪᴛ...</b>')
+    total_users = await db.total_users_count()
+    totl_chats = await db.total_chat_count()
+    files = await Media.count_documents()
+    size = await db.get_db_size()
+    free = 536870912 - size
+    size = get_size(size)
+    free = get_size(free)
+    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
