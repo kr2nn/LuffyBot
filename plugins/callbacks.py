55aeebe8088372a97f32f336bf655c9e35304915
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-05 01:40:55 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -35,10 +35,12 @@ async def button(bot, update):
            reply_markup=Translation.BUTTONS  # Optional: Add reply markup if needed
        )
     elif update.data == "demopic":
-       await bot.edit_message_media(
-           media=types.InputMediaPhoto(Translation.PAYMENT_QR), 
-           caption=Translation.QR_TEXT, 
-           reply_markup=Translation.BUTTONS
-       )                                       
+      await bot.edit_message_media(
+          chat_id=update.message.chat.id,
+          message_id=update.message.message_id,
+          media=types.InputMediaPhoto(media=Translation.PAYMENT_QR),
+          caption=Translation.QR_TEXT,
+          reply_markup=Translation.BUTTONS
+      )                                       
     elif "close" in update.data:
         await update.message.delete(True)
