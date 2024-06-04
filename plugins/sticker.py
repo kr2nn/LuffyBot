cbf536fab4dca3e84f71073f94a9f6775ff5262c
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-04 20:43:19 +0530

    Delete plugins/sticker.py

--- plugins/sticker.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("stickerid") & filters.private)
-async def stickerid(bot, message):
-    nikz_msg = await bot.ask(stickerid = message.from_user.id, text = "Now Send Me Your Sticker")
-    if nikz_msg.sticker:
-        await nikz_msg.reply_text(f"**Sticker ID is**  \n `{s_msg.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{s_msg.sticker.file_unique_id}`")
-    else: 
-        await nikz_msg.reply_text("Oops !! Not a sticker file")
