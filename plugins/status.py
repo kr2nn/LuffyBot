b02978781b93045cdac69efa31f615f2f7a752a2
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-04 19:21:28 +0530

    Update status.py

--- plugins/status.py
+++ plugins/status.py
@@ -7,7 +7,7 @@ from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, Peer
 from plugins.database.database import db
 from plugins.config import Config
 
-@Client.on_message(filters.private & filters.command("status"))
+@Client.on_message(filters.private & filters.command("stats"))
 async def status(bot, update):
     total_users = await db.total_users_count()
     text = "**Bot Status**\n"
@@ -17,11 +17,3 @@ async def status(bot, update):
         quote=True,
         disable_web_page_preview=True
     )
-@Client.on_message(filters.command('stats') & filters.incoming)
-async def get_ststs(bot, message):
-    rju = await message.reply('<b>Pʟᴇᴀꜱᴇ Wᴀɪᴛ...</b>')
-    total_users = await db.total_users_count()
-    free = 536870912 - size
-    size = get_size(size)
-    free = get_size(free)
-    await rju.edit(Transaction.STATUS_TXT.format(total_users, free))
