bd230ec32a9ca69d3584dfed16e1e8fc0ec57c56
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-05 01:18:26 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 @Client.on_callback_query()
-async def button(bot, update):
+async def button(bot, update, CallbackQuery):
     if update.data == "home":
         await update.message.edit_text(
             text=Translation.START_TEXT.format(update.from_user.mention),
