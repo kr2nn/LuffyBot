751b30c84f163675bfa3f0ae59038a41ca6b8f37
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-04 20:43:43 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -95,13 +95,13 @@ async def broadcast_(c, m):
     
     if failed == 0:
         await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            text=f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴘʟᴇᴛᴇᴅ ɪɴ `{completed_in}`\n\nTᴏᴛᴀʟ Uꜱᴇʀ'ꜱ {total_users}.\nTᴏᴛᴀʟ Dᴏɴᴇ {done}, {success} Sᴜᴄᴄᴇꜱꜱ Aɴᴅ {failed} Fᴀɪʟᴇᴅ.",
             quote=True
         )
     else:
         await m.reply_document(
             document='broadcast.txt',
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+            caption=f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴘʟᴇᴛᴇᴅ ɪɴ `{completed_in}`\n\nTᴏᴛᴀʟ Uꜱᴇʀ'ꜱ {total_users}.\nTᴏᴛᴀʟ Dᴏɴᴇ {done}, {success} Sᴜᴄᴄᴇꜱꜱ Aɴᴅ {failed} Fᴀɪʟᴇᴅ.",
             quote=True
         )
     
