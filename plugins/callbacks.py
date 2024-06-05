f07cc2355de5fe96cd0a422d422f297d97c2d72d
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 01:52:22 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -16,28 +16,17 @@ logger = logging.getLogger(__name__)
 @Client.on_callback_query()
 async def button(bot, update):
     if update.data == "home":
-      buttons = [[  
-              InlineKeyboardButton('രോമാഞ്ചം പ്രീമിയം 🔕', callback_data='premium')
-              ],[
-              InlineKeyboardButton('✘ Cʟᴏsᴇ', callback_data='close')
-              ]]
-        await update.edit_message_media(InputMediaPhoto(random.choice(PICS), Translation.START_TEXT.format(user=update.from_user.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))  
+        await update.message.edit_text(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.START_BUTTONS,
+            disable_web_page_preview=True
+        )
     elif update.data == "premium":
-      buttons = [[
-        InlineKeyboardButton('💰 Pᴀʏ 100', callback_data='payment')
-        ],[
-         InlineKeyboardButton('Dᴇᴍᴏ Pɪᴄꜱ 🏞️', callback_data='demopic')
-        ],[ 
-        InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='home'),   
-        InlineKeyboardButton('✘ Cʟᴏsᴇ', callback_data='close')
-        ]]
-        await update.edit_message_media(InputMediaPhoto(random.choice(PICS), Translatio.DETAILS_TEXT.format(user=update.from_user.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
-    elif update.data == "demopic":
-       await bot.send_photo(
-              chat_id=update.message.chat.id,
-              photo=Translation.DEMO_PIC,  # Assuming PAYMENT_QR contains the file ID or file path of the photo
-              caption=Translation.DEMO_TEXT,  # Caption for the photo
-             )
+        await update.message.edit_text(
+            text=Translation.DETAILS_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.PREMIUM_BUTTONS,
+            disable_web_page_preview=True
+        )
     elif update.data == "payment":
         buttons = [[
         InlineKeyboardButton('💰 Pᴀʏ 100', callback_data='payment')
