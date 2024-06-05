a8c34a284b1380147db18fc5c70eb896cfe26944
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 00:29:54 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -16,17 +16,22 @@ logger = logging.getLogger(__name__)
 @Client.on_callback_query()
 async def button(bot, update):
     if update.data == "home":
-        await update.message.edit_text(
-            text=Translation.START_TEXT.format(update.from_user.mention),
-            reply_markup=Translation.START_BUTTONS,
-            disable_web_page_preview=True
-        )
+      buttons = [[  
+              InlineKeyboardButton('രോമാഞ്ചം പ്രീമിയം 🔕', callback_data='premium')
+              ],[
+              InlineKeyboardButton('✘ Cʟᴏsᴇ', callback_data='close')
+              ]]
+      await update.edit_message_media(InputMediaPhoto(random.choice(PICS), Translation.START_TEXT.format(update.from_user.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons)  
     elif update.data == "premium":
-        await update.message.edit_text(
-            text=Translation.DETAILS_TEXT.format(update.from_user.mention),
-            reply_markup=Translation.PREMIUM_BUTTONS,
-            disable_web_page_preview=True
-        )
+      buttons = [[
+        InlineKeyboardButton('💰 Pᴀʏ 100', callback_data='payment')
+        ],[
+         InlineKeyboardButton('Dᴇᴍᴏ Pɪᴄꜱ 🏞️', callback_data='demopic')
+        ],[ 
+        InlineKeyboardButton('« Bᴀᴄᴋ', callback_data='home'),   
+        InlineKeyboardButton('✘ Cʟᴏsᴇ', callback_data='close')
+        ]]
+        await update.edit_message_media(InputMediaPhoto(random.choice(PICS), Translatio.DETAILS_TEXT.format(update.from_user.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons)
   #  elif update.data == "payment":
     #   await update.message.delete()
     # Send the photo
@@ -37,13 +42,11 @@ async def button(bot, update):
         #   reply_markup=Translation.BUTTONS  # Optional: Add reply markup if needed
       # )
     elif update.data == "demopic":
-      await bot.edit_message_media(
-          chat_id=update.message.chat.id,
-          message_id=update.message.message_id,
-          media=types.InputMediaPhoto(media=Translation.PAYMENT_QR),
-          caption=Translation.QR_TEXT,
-          reply_markup=Translation.BUTTONS
-      )
+        await bot.send_photo(
+              chat_id=update.message.chat.id,
+              photo=Translation.DEMO_PIC,  # Assuming PAYMENT_QR contains the file ID or file path of the photo
+              caption=Translation.DEMO_TEXT,  # Caption for the photo
+             )
     elif update.data == "payment":
         buttons = [[
         InlineKeyboardButton('💰 Pᴀʏ 100', callback_data='payment')
