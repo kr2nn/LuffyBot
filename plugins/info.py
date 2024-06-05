4e4314db77e7a883fadfb0e799686116becd80d4
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 00:21:19 +0530

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -2,6 +2,6 @@ import re, time
 from os import environ
 from plugins.script import Translation
 
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/13138bb072d6ae54386d4.jpg')).split()
 
 QR_PIC = (environ.get('QR_PIC' ,'https://telegra.ph/file/4acfd5a112e10ba0bf34f.jpg')).split()
