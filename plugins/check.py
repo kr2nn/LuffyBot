027e75cbb6a8a715b5209880a929bf35bcc15865
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 16:36:47 +0530

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -1,5 +1,10 @@
+from pyrogram import Client, filters
+from plugins.config import Config
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
 
 
-
-@Client.on_message(filters.command(["start"]) & filters.private)
-async def start(bot, update):
+@Client.on_message(filters.command(["video"]) & filters.private)
+async def video(bot, update):
+  awit update.reply_video(
+    
+  )
