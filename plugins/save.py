2e97a0c89250fbe5dec501d1dcea90abb117f604
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 15:02:54 +0530

    Update save.py

--- plugins/save.py
+++ plugins/save.py
@@ -46,8 +46,6 @@ async def save_photo(bot, update):
                   disable_web_page_preview=True,
                   quote=True
               )
-            except Exception as error:
-            print(error)
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")
     await add_user_to_database(bot, update)
