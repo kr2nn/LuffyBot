19f87055c042779067119f58783ac30ea52ca73c
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 10:43:47 +0530

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -4,4 +4,6 @@ from plugins.script import Translation
 
 PICS = (environ.get('PICS' ,'https://telegra.ph/file/13138bb072d6ae54386d4.jpg')).split()
 
+PIC = (environ.get('PICS' ,'https://telegra.ph/file/3e68bfd9242aefd68cb70.jpg')).split()
+
 QR_PIC = (environ.get('QR_PIC' ,'https://telegra.ph/file/4acfd5a112e10ba0bf34f.jpg')).split()
