ea539bf06fed1f981e5602885e7a9873fb3165fb
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 14:27:05 +0530

    Update save.py

--- plugins/save.py
+++ plugins/save.py
@@ -60,7 +60,7 @@ async def save_photo(bot, update):
     )
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.SAVED_CUSTOM_THUMB_NAIL,
+        text=Translation.PROOF_UPLOAD,
         reply_to_message_id=update.id
     )
     await db.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
