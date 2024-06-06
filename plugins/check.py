dfcb06a17693e659631cd56249d09472da9e56f6
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 16:37:14 +0530

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -5,6 +5,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 
 @Client.on_message(filters.command(["video"]) & filters.private)
 async def video(bot, update):
-  awit update.reply_video(
+  await update.reply_video(
     
   )
