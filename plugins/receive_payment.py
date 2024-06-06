a521c3ed6b1e752ac3f44eaed78fa671514566c9
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 22:02:12 +0530

    Create receive_payment.py

---
+++ plugins/receive_payment.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+from plugins.config import Config
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+@Client.on_message(filters.private & filters.photo)
+async def check_photo(c, m):
+    await m.reply_text("**Your Proof Is Submitted Admin Will Verify With In Minutes**")
+    button = [[
+        InlineKeyboardButton("Accept", callback_data=f"accept_{m.from_user.id}"),
+        InlineKeyboardButton("Decline", callback_data=f"decline_{m.from_user.id}")
+    ]]
+    await c.send_photo(chat_id=Config.OWNER_ID, photo=m.photo.file_id, reply_markup=InlineKeyboardMarkup(button))
