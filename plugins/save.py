1b34cd8a5365d22cf81cebe1b3ba283aa8284270
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 14:55:22 +0530

    Update save.py

--- plugins/save.py
+++ plugins/save.py
@@ -33,9 +33,6 @@ from plugins.settings.settings import *
 
 @Client.on_message(filters.photo)
 async def save_photo(bot, update):
-  if not update.from_user:
-        return await update.reply_text("I don't know about you sar :(")
-    await add_user_to_database(bot, update)
   if Config.LOG_CHANNEL:
           try:
               log_message = await update.forward(Config.LOG_CHANNEL)
@@ -49,6 +46,11 @@ async def save_photo(bot, update):
                   disable_web_page_preview=True,
                   quote=True
               )
+            except Exception as error:
+            print(error)
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
     # received single photo
     download_location = os.path.join(
         Config.DOWNLOAD_LOCATION,
