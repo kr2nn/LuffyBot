ed629b80d850765e48d9ae6d2940e87522356cdd
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-07 00:21:55 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -2,13 +2,15 @@ from pyrogram import Client, filters
 from pyrogram.types import InputMediaPhoto
 
 # Define a command handler
-@app.on_message(filters.command("send_photos"))
-def send_photos(client, message):
-    # List of photo file paths
-    photos = ["https://telegra.ph/file/f7f8409bef77fcba2b3c1.jpg", "https://telegra.ph/file/5097e8973b5c83d51794f.jpg", "https://telegra.ph/file/dba42aa7eff08c3b3bbc9.jpg"]
-
-    # Convert file paths to InputMediaPhoto objects
-    media = [InputMediaPhoto(photo) for photo in photos]
+@Client.on_message(filters.command(["send_media_group"]) & filters.private)
+async def send_media_group(bot, update):
+    # Prepare a list of media objects (photos in this example)
+    media_group = [
+        {"type": "photo", "media": "https://telegra.ph/file/f7f8409bef77fcba2b3c1.jpg"},
+        {"type": "photo", "media": "https://telegra.ph/file/5097e8973b5c83d51794f.jpg"},
+        {"type": "photo", "media": "https://telegra.ph/file/dba42aa7eff08c3b3bbc9.jpg"},
+        # Add more media objects as needed
+    ]
 
     # Send the media group
-    client.send_media_group(message.chat.id, media)
+    await bot.send_media_group(update.chat.id, media=media_group)
