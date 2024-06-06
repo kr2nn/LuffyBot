3c2a76d6c2081cab0dc759ac4e3d6c91e599e7f8
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-06-06 16:55:46 +0530

    Delete plugins/check.py

--- plugins/check.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from plugins.config import Config
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
-
-
-@Client.on_message(filters.command(["video"]) & filters.private)
-async def video(bot, update):
-  await update.reply_video("BAACAgUAAxkBAAMrZmGYiHm9S7IjA44rNlbTwsLRRCwAAssLAALGdiFV0RUWONnocPgeBA")
